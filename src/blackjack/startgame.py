2eba111db13073e977d60605e8ca0de9a758e1b2
Brian Vegetabile <bvegetabile@gmail.com>
2025-01-14 20:42:38 -0800

    adds ui for continued gameplay

--- src/blackjack/startgame.py
+++ src/blackjack/startgame.py
@@ -22,6 +22,13 @@ RANK_SCORES = {
 }
 
 
+STAND_INPUTS = [
+    'stand',
+    's',
+    '1'
+]
+
+
 class Player:
     def __init__(self, player_id=None, player_type=None):
         self.player_id = player_id
@@ -77,90 +84,97 @@ class Player:
 class BlackjackGame:
     def __init__(self, nplayers=1, init_shuffled=True):
         self.nplayers = nplayers
-        self.player_list = []
-        # TODO Add Player Seat Randomization.
-        for player_id in range(self.nplayers):
-            if player_id == 0:
-                self.player_list.append(
-                    Player(
-                        player_id=player_id+1, 
-                        player_type="normal"
+        self.game_state = None
+        while self.game_state is None:
+            self.player_list = []
+            # TODO Add Player Seat Randomization.
+            for player_id in range(self.nplayers):
+                if player_id == 0:
+                    self.player_list.append(
+                        Player(
+                            player_id=player_id+1, 
+                            player_type="normal"
+                        )
                     )
-                )
-            else: 
-                self.player_list.append(
-                    Player(
-                        player_id=player_id+1, 
-                        player_type="computer"
+                else: 
+                    self.player_list.append(
+                        Player(
+                            player_id=player_id+1, 
+                            player_type="computer"
+                        )
                     )
-                )
-        self.player_list.append(
-            Player(
-                player_id=None,
-                player_type="dealer"
-                )
-        )
+            self.player_list.append(
+                Player(
+                    player_id=None,
+                    player_type="dealer"
+                    )
+            )
 
-        # Initialize deck of cards + shuffle.
-        self.deck = DeckOfCards()
-        if init_shuffled:
-            self.deck.shuffle()
+            # Initialize deck of cards + shuffle.
+            self.deck = DeckOfCards()
+            if init_shuffled:
+                self.deck.shuffle()
 
-        # deal cards
-        for i in range(2):
+            # deal cards
+            for i in range(2):
+                for player in self.player_list:
+                    new_card = self.deck.get_card()
+                    player.add_card_to_hand(new_card)
+
+            # Show table:
             for player in self.player_list:
+                if player.player_id is not None: 
+                    hfc = False
+                else:
+                    hfc = True
+                player.print_hand(hide_first_card=hfc)
+
+            # Start game:
+            while self.player_list[0].last_player_action != 'stand':
+                player_action = input("Whats your move, type: '1' or 'stand', [return] for 'hit'\n>>> ")
+                print(80*'.')
+                self.player_list[0].last_player_action = player_action
+                if player_action.lower() in STAND_INPUTS:
+                    break
                 new_card = self.deck.get_card()
-                player.add_card_to_hand(new_card)
+                self.player_list[0].add_card_to_hand(new_card)
+                self.player_list[0].print_hand()
+                current_score = self.player_list[0].score_hand()
+                if current_score > 21:
+                    print("Bust.")
+                    break
 
-        # Show table:
-        for player in self.player_list:
-            if player.player_id is not None: 
-                hfc = False
-            else:
-                hfc = True
-            player.print_hand(hide_first_card=hfc)
+            # Dealer actions 
+            while self.player_list[-1].score_hand() <= 17:
+                new_card = self.deck.get_card()
+                self.player_list[-1].add_card_to_hand(new_card)
+                self.player_list[-1].print_hand(hide_first_card=False)
+                current_score = self.player_list[-1].score_hand()
+                if current_score > 21:
+                    print("Bust.")
+                    break
 
-        # Start game:
-        while self.player_list[0].last_player_action != 'stand':
-            player_action = input("Whats the move:")
-            self.player_list[0].last_player_action = player_action
-            if player_action == 'stand':
-                break
-            new_card = self.deck.get_card()
-            self.player_list[0].add_card_to_hand(new_card)
-            self.player_list[0].print_hand()
-            current_score = self.player_list[0].score_hand()
-            if current_score > 21:
-                print("Bust.")
-                break
-
-        # Dealer actions 
-        while self.player_list[-1].score_hand() <= 17:
-            new_card = self.deck.get_card()
-            self.player_list[-1].add_card_to_hand(new_card)
             self.player_list[-1].print_hand(hide_first_card=False)
-            current_score = self.player_list[-1].score_hand()
-            if current_score > 21:
-                print("Bust.")
+
+            # Print results. 
+            print(80*'-')
+            dealer_score = self.player_list[-1].score_hand()
+            for player in self.player_list[:-1]:
+                player_score = player.score_hand()
+                score_results = f"| Hand Score {player_score}, Dealer Hand {dealer_score}."
+                if player_score > dealer_score and player_score <= 21 and dealer_score <= 21:
+                    print(f"Player {player.player_id}: Wins. {score_results}")
+                elif player_score <= 21 and dealer_score > 21:
+                    print(f"Player {player.player_id}: Wins. {score_results}")
+                elif player_score == dealer_score and player_score <= 21: 
+                    print(f"Player {player.player_id}: Pushes. {score_results}")
+                else:
+                    print(f"Player {player.player_id}: Loses. {score_results}")
+            print(80*'-')
+        
+            player_next_game = input("Press [return] to play again. Type [q] to quit.>>> ")
+            if player_next_game == 'q':
                 break
-
-        self.player_list[-1].print_hand(hide_first_card=False)
-
-        # Print results. 
-        print(80*'-')
-        dealer_score = self.player_list[-1].score_hand()
-        for player in self.player_list[:-1]:
-            player_score = player.score_hand()
-            score_results = f"| Hand Score {player_score}, Dealer Hand {dealer_score}."
-            if player_score > dealer_score and player_score <= 21 and dealer_score <= 21:
-                print(f"Player {player.player_id}: Wins. {score_results}")
-            elif player_score <= 21 and dealer_score > 21:
-                print(f"Player {player.player_id}: Wins. {score_results}")
-            elif player_score == dealer_score and player_score <= 21: 
-                print(f"Player {player.player_id}: Pushes. {score_results}")
-            else:
-                print(f"Player {player.player_id}: Loses. {score_results}")
-        print(80*'-')
 
 
 
@@ -197,5 +211,4 @@ def startgame(
     )
 
     _ = BlackjackGame(nplayers=nplayers)
-
 
